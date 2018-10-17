6ba70e56d9fbbc6b2044b3be520b765672300855
Issac-Aguilar2018 <43555909+Issac-Aguilar2018@users.noreply.github.com>
2018-10-16 20:12:30 -0700

    Unidad 3
    Tarea 3

---
+++ E3-2_AguilarZendejasIssacIssai/E3-2_AguilarZendejasIssacIssai/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_2_AguilarZendejasIssacIssai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Metodos Queue, La cola tiene un comportamiento contrario a la pila.
+            Queue Cola = new Queue(); ///Enqueue sirve para colocar los elementos al final del queue 
+            Cola.Enqueue("Aguilar");
+            Cola.Enqueue(19);
+            Cola.Enqueue("Issac");
+            Cola.Enqueue('i');
+            foreach (var x in Cola)///Imprimir la cola 
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\nMetodo Dequeue: " + Cola.Dequeue());///Elemento primero en la cola
+            foreach (var x in Cola)///Ahora va imprimir con 3 elementos, porque ya que el primer elemento que ingresa a la cola es el primer elemento que sale.
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\nElementos en el queue: " + Cola.Count);///Elementos que hay en el queu          
+            Console.WriteLine("Elemento al principio: " + Cola.Peek());///Devuelve un elemento al principio sin eliminarlo             
+            bool TF = Cola.Contains(19);
+            Console.WriteLine("Elemento [19] se encuentra en la cola: " + TF);///Saber si el elemento se encuentra en la cola                                                      
+            Console.ReadKey();
+        }
+    }
+}
